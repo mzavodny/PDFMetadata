bfcf3759ef573ccf77a3114c727f4a17bfe187d0
mzavodny <mzavodny@centrum.cz>
2024-03-01 11:22:46 +0100

    Add logic for fileupload, logging, pdfa validation

--- PDFMetadata.UI/Default.aspx.cs
+++ PDFMetadata.UI/Default.aspx.cs
@@ -1,22 +1,89 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using Codeuctivity;
+using PDFMetadata.UI.Logger;
+using System;
+using System.IO;
+using System.Threading.Tasks;
 using System.Web.UI;
-using System.Web.UI.WebControls;
 
 namespace PDFMetadata.UI
 {
     public partial class _Default : Page
     {
+        /// <summary>
+        /// Folder in root of the application for for temp pdfa files
+        /// </summary>
+        const string PATH_FOR_UPLOAD = "~/TempFiles/";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        const string SUPPORTED_CONTENT_TYPE = "application/pdf";
+        
+        /// <summary>
+        /// Folder in root of the application for logging exceptions
+        /// </summary>
+        const string PATH_FOR_LOGS = "~/Logs/";
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
         }
 
-        protected void btnUploadFile_Click(object sender, EventArgs e)
-        { 
-        
+        protected async void btnUploadFile_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                await PdfaFileValidation();
+            }
+            catch (Exception ex)
+            {
+                FileLogger.Log(ex, Server.MapPath(PATH_FOR_LOGS));
+                lblMessage.Text = "Uncaught exception, please contact support.";
+                lblMessage.ForeColor = System.Drawing.Color.Red;
+            }
+        }
+
+        /// <summary>
+        /// Validation of the pdf file and customization UI according to the result.
+        /// </summary>
+        /// <returns></returns>
+        private async Task PdfaFileValidation()
+        {
+            if (fileUpload.HasFile && fileUpload.PostedFile.ContentType == SUPPORTED_CONTENT_TYPE)
+            {
+                GetFilePath(out string newFilePath);
+
+                fileUpload.SaveAs(newFilePath);
+
+                var profileName = await GetValidationProfileName(newFilePath);
+
+                File.Delete(newFilePath);
+
+                lblMessage.Text = $"File is valid ({profileName}).";
+                lblMessage.ForeColor = System.Drawing.Color.Green;
+            }
+            else
+            {
+                lblMessage.Text = "Please select a file [.pdf] to upload.";
+                lblMessage.ForeColor = System.Drawing.Color.Red;
+            }
+        }
+
+        private void GetFilePath(out string newFilePath)
+        {
+            if (!Directory.Exists(Server.MapPath(PATH_FOR_UPLOAD)))
+                Directory.CreateDirectory(Server.MapPath(PATH_FOR_UPLOAD));
+
+            newFilePath = Server.MapPath(PATH_FOR_UPLOAD + fileUpload.FileName);
+        }
+
+        private async Task<string> GetValidationProfileName(string newFilePath)
+        {
+            using (var pdfAValidator = new PdfAValidator())
+            {
+                var result = await pdfAValidator.ValidateWithDetailedReportAsync(newFilePath);
+                return result.Jobs.Job.ValidationReport.ProfileName;
+            }
         }
     }
 }
---
+++ PDFMetadata.UI/Logger/FileLogger.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace PDFMetadata.UI.Logger
+{
+    /// <summary>
+    /// Logging to root file 
+    /// </summary>
+    public class FileLogger
+    {
+        public static void Log(Exception exception, string path)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            do
+            {
+                sb.Append("Exception Type" + Environment.NewLine);
+                sb.Append(exception.GetType().Name);
+                sb.Append(Environment.NewLine + Environment.NewLine);
+                sb.Append("Message" + Environment.NewLine);
+                sb.Append(exception.Message + Environment.NewLine + Environment.NewLine);
+                sb.Append("Stack Trace" + Environment.NewLine);
+                sb.Append(exception.StackTrace + Environment.NewLine + Environment.NewLine);
+                exception = exception.InnerException;
+            } while (exception != null);
+
+            if (!Directory.Exists(path))
+                Directory.CreateDirectory(path);
+
+            File.WriteAllText(path + $"{DateTime.Now.ToString("yyyyMMddTHHmmss")}.txt", sb.ToString());
+        }
+    }
+}
